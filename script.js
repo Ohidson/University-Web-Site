4fce146fa9e0cb30adc7095176e5e1645b270ca9
Ohidson <ohidson@gmail.com>
2024-06-10 14:55:05 -0900

    commited

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const menu_icon = document.getElementById("menu_icon");
-const menu_bar = document.getElementById("menu_icon");
+const menu_bar = document.getElementById("menu_bar");
+const close_icon = document.getElementById("close_icon");
 
-function clickFunc(){
-  menu_bar.style.right = 0
-}
+menu_icon.addEventListener('click', function(){
+  menu_bar.style.right = "0px"
+})
+
+close_icon.addEventListener('click', function(){
+  menu_bar.style.right = "-250px"
+})
